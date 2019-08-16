7ee0acc0746640ac15625ef990f9cc79e599c405
xuetaoshi <samuelshi1988@gmail.com>
2019-08-16 13:20:53 -0400

    removed unnecessary files

--- customerdal/src/main/java/com/xuetaoshi/customer/dal/repos/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.xuetaoshi.customer.dal.repos;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.xuetaoshi.customer.dal.entities.Customer;
-
-public interface CustomerRepository extends CrudRepository<Customer, Long> {
-
-}
--- studentdal/src/main/java/com/xuetaoshi/student/dal/repos/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.xuetaoshi.student.dal.repos;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.xuetaoshi.student.dal.entities.Student;
-
-public interface StudentRepository extends CrudRepository<Student, Long> {
-
-}
